91e2bff938fce1f0b673f5edf5b2f04262550046
Lucas Marques <lmlira29@gmail.com>
2025-06-18 00:53:02 -0300

    Rota de cadastro funcionando com salvamento de senha has

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 import express from 'express';
-import routerCadastro from './src/rotas/controllers/public.js'
-
+import routerCadastro from './src/rotas/controllers/public.js';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
 
 
-// Rota para o routerCadastro de usuários
+app.use(cors());
+// Configuração do CORS para permitir requisições de qualquer origem
+cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true // Permite credenciais (cookies, headers de autenticação, etc.)})
+});
+// Rota para o cadastro de usuários
 app.use('/', routerCadastro);
 
 
-app.listen(3000, (() => {
-    console.log("Servidor rodando!")
-}))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
--- src/rotas/controllers/public.js
+++ src/rotas/controllers/public.js
@@ -1,35 +1,47 @@
-import express from 'express';
+import express from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
-
 const router = express.Router();
 
-router.post('/cadastro', async (req, res) => {
+try {
+    // Rota para o cadastro de usuários
+  router.post("/cadastro", async (req, res) => {
     const dadosUsers = req.body;
-    console.log(dadosUsers);
+
+    // Cria um novo usuário no banco de dados com a senha criptografada
+    const saltRounds = await bcrypt.genSalt(10);
+    const hasPassword = await bcrypt.hash(dadosUsers.password, saltRounds);
 
     const newUser = await prisma.user.create({
-        data: {
-            name:dadosUsers.name,
-            email: dadosUsers.email,
-            password: dadosUsers.password,
-        },
+      data: {
+        name: dadosUsers.name,
+        email: dadosUsers.email,
+        password: hasPassword,
+      },
     });
-    if (!newUser) {
-        return res.status(400).json({ mensagem: "Erro ao cadastrar usuário!" });
+    // Respsonde com uma mensagem de sucesso ou erro ao cadastrar o usuário
+    if (newUser) {
+      return res
+        .status(201)
+        .json({ mensagem: "Usuário cadastrado com sucesso!" });
+    } else {
+      return res.status(500).json({ mensagem: "Erro ao cadastrar usuário!" });
     }
-    
-    res.status(200).json({
-        mensagem: "Dados recebidos com sucesso!",
-        dados: dadosUsers
-    });
-});
+  });
 
-router.get('/login', (req, res) => {
+  // Rota para o login de usuários
+  router.get("/login", (req, res) => {
     const loginData = req.body;
     console.log(loginData);
-    res.status(200).json({mensagem: "Login realizado com sucesso!", dados: loginData});
-})
+    res
+      .status(200)
+      .json({ mensagem: "Login realizado com sucesso!", dados: loginData });
+  });
+} catch (error) {
+    console.error("Erro ao processar a requisição:", error);
+    res.status(500).json({ mensagem: "Erro interno do servidor." });
+}
 
 export default router;
